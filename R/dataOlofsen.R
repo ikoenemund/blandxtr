beab4fe85c864fcd17d57a22b525712ad61b012a
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-01-27 09:23:45 +0100

    Added example data from Olofsen et al. 2015 (including documentation)

---
+++ R/dataOlofsen.R
@@ -0,0 +1,16 @@
+#' Results of repeated measurements
+#'
+#' Example dataset of Olofsen et al. containing results of
+#' 15 repeated measurements (second and third column) with
+#' two different tools in 20 individuals (first column).
+#' The variables are as follows:
+#'
+#' \describe{
+#'   \item{patient}{patient ID}
+#'   \item{measurementX}{results of measuring instrument X}
+#'   \item{measurementY}{results of measuring instrument Y}
+#' }
+#'
+#' @format A data frame with 300 rows and 3 variables
+#' @source \url{https://sec.lumc.nl/method_agreement_analysis/sim.tx}
+"dataOlofsen"
