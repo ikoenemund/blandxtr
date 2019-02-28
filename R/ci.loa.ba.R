956ae5167679a2b2af0cb6f84a96ac22ebf78813
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-02-28 15:53:55 +0100

    Added documentation

--- R/ci.loa.ba.R
+++ R/ci.loa.ba.R
@@ -1,6 +1,23 @@
-# Bland Altman method: calculation of confidence intervals (CI) for limits of agreement
+#' @title 95\%-confidence intervals for LoA (Bland Altman)
+#'
+#' @description \code{calc_ci_loa_ba} returns 95\%-confidence intervals (95\%-CI)
+#' for limits of agreement (LoA) based on a method proposed
+#' by Bland and Altman (1999).
+#'
+#' @author Inga Koenemund \email{inga.koenemund@web.de}
+#'
+#' @param loa_l lower limit of agreement
+#' @param loa_u upper limit of agreement
+#' @param var_loa variance of limits of agreement
+#'
+#' @return A list with the following elements is returned
+#' \itemize{
+#'  \item{\code{ci_l_loa_l_ba}} {lower limit of 95\%-CI for lower LoA}
+#'  \item{\code{ci_u_loa_l_ba}} {upper limit of 95\%-CI for lower LoA}
+#'  \item{\code{ci_l_loa_u_ba}} {lower limit of 95\%-CI for upper LoA}
+#'  \item{\code{ci_u_loa_u_ba}} {upper limit of 95\%-CI for upper LoA}
+#' }
 
-# using function
 calc_ci_loa_ba <- function(loa_l, loa_u, var_loa) {
   ci_l_loa_l_ba <- loa_l-(1.96*(sqrt(var_loa)))
   ci_u_loa_l_ba <- loa_l+(1.96*(sqrt(var_loa)))
