a13eef482a1f91957b5ba041046e26c8e2b2cb02
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-03-11 17:19:57 +0100

    Improved runtime by using lapply instead of for-loops.

--- R/ci.loa.bt.R
+++ R/ci.loa.bt.R
@@ -28,43 +28,111 @@
 
 calc_ci_loa_bt <- function(bt, input_dt, biasMod, loa_l, loa_u, var_loa) {
   source("R/blandxtrMain.pre.R")
-  # prepare matrix for storing results of bootstrapping
+
+  # # prepare matrix for storing results of bootstrapping
+  # s <- matrix(NA, nrow = bt, ncol = 5)
+  # colnames(s) = (c("lower LoA", "upper LoA", "SD LoA", "z_l", "z_u"))
+  #
+  # # generate B bootstrap samples of input_dt
+  # for (i in 1:bt){
+  #   # boot <- sample(nrow(input_dt), replace = TRUE)
+  #   # boot_dt <- as.data.table(input_dt[boot])
+  #   ### TEST
+  #   # boot_dt <- as.data.table(sample(input_dt, 300, replace = TRUE))
+  #   start_time <- Sys.time()
+  #   boot_dt <- input_dt[sample(nrow(input_dt), 300, replace = TRUE), ]
+  #   end_time <- Sys.time()
+  #   time_sample <- end_time - start_time
+  #   ###
+  #   start_time <- Sys.time()
+  #   boot <- blandxtrMain_pre(bt, boot_dt, biasMod)
+  #   end_time <- Sys.time()
+  #   time_ba <- end_time - start_time
+  #
+  #   start_time <- Sys.time()
+  #   s[i,1] <- boot$loa$loa_l
+  #   s[i,2] <- boot$loa$loa_u
+  #   s[i,3] <- sqrt(boot$var_loa)
+  #   s[i,4] <- (boot$loa$loa_l-loa_l)/var_loa
+  #   s[i,5] <- (boot$loa$loa_u-loa_u)/var_loa
+  #   end_time <- Sys.time()
+  #   time_matrix <- end_time - start_time
+  # }
+
+  #  sampling
+  start_time <- Sys.time()
+  boot_samp <- vector("list", bt)
+  i <- 1:bt
+  sample_dt <- function(i, input_dt){
+    boot_dt <- input_dt[sample(nrow(input_dt), nrow(input_dt), replace = TRUE), ]
+    boot_samp[[i]] <- boot_dt
+  }
+  boot_samp <- lapply(i, sample_dt, input_dt=input_dt)
+  rm(i)
+  end_time <- Sys.time()
+  time_sample <- end_time - start_time
+
+
+  # ba per sample
+  start_time <- Sys.time()
+
+  boot <- lapply(boot_samp, blandxtrMain_pre, bt=bt, biasMod=biasMod)
+
+  end_time <- Sys.time()
+  time_ba <- end_time - start_time
+
+  # initialize and fill matrix
+  start_time <- Sys.time()
+
   s <- matrix(NA, nrow = bt, ncol = 5)
   colnames(s) = (c("lower LoA", "upper LoA", "SD LoA", "z_l", "z_u"))
 
-  # generate B bootstrap samples of input_dt
-  for (i in 1:bt){
-    # boot <- sample(nrow(input_dt), replace = TRUE)
-    # boot_dt <- as.data.table(input_dt[boot])
-    ### TEST
-    # boot_dt <- as.data.table(sample(input_dt, 300, replace = TRUE))
+  for(r in 1:bt){
     start_time <- Sys.time()
-    boot_dt <- input_dt[sample(nrow(input_dt), 300, replace = TRUE), ]
-    end_time <- Sys.time()
-    time_sample <- end_time - start_time
-    ###
-    start_time <- Sys.time()
-    boot <- blandxtrMain_pre(bt, boot_dt, biasMod)
-    end_time <- Sys.time()
-    time_ba <- end_time - start_time
-
-    start_time <- Sys.time()
-    s[i,1] <- boot$loa$loa_l
-    s[i,2] <- boot$loa$loa_u
-    s[i,3] <- sqrt(boot$var_loa)
-    s[i,4] <- (boot$loa$loa_l-loa_l)/var_loa
-    s[i,5] <- (boot$loa$loa_u-loa_u)/var_loa
+    s[r,1] <- boot[[r]]$loa$loa_l
+    s[r,2] <- boot[[r]]$loa$loa_u
+    s[r,3] <- sqrt(boot[[r]]$var_loa)
+    s[r,4] <- (boot[[r]]$loa$loa_l-loa_l)/var_loa
+    s[r,5] <- (boot[[r]]$loa$loa_u-loa_u)/var_loa
+  }
+  rm(r)
     end_time <- Sys.time()
     time_matrix <- end_time - start_time
-  }
 
   start_time <- Sys.time()
   ci_l_loa_l_bt <- loa_l-(sqrt(var_loa)*quantile(s[,4],0.975))
   ci_u_loa_l_bt <- loa_l-(sqrt(var_loa)*quantile(s[,4],0.025))
   ci_l_loa_u_bt <- loa_u-(sqrt(var_loa)*quantile(s[,5],0.975))
   ci_u_loa_u_bt <- loa_u-(sqrt(var_loa)*quantile(s[,5],0.025))
+  rm(s)
   end_time <- Sys.time()
   time_quantiles <- end_time - start_time
+
+  ### TEST
+
+  # # initialize and fill matrix
+  # start_time <- Sys.time()
+  #
+  # s <- matrix(NA, nrow = bt, ncol = 5)
+  # colnames(s) = (c("lower LoA", "upper LoA", "SD LoA", "z_l", "z_u"))
+  #
+  # r <- 1:bt
+  # # r: number of row to fill
+  # fill_matrix <- function (r){
+  #   s[r,1] <- boot[[r]]$loa$loa_l
+  #   s[r,2] <- boot[[r]]$loa$loa_u
+  #   s[r,3] <- sqrt(boot[[r]]$var_loa)
+  #   s[r,4] <- (boot[[r]]$loa$loa_l-loa_l)/var_loa
+  #   s[r,5] <- (boot[[r]]$loa$loa_u-loa_u)/var_loa
+  # }
+  # s <- apply(r, FUN = fill_matrix)
+  # rm(r)
+  #
+  # end_time <- Sys.time()
+  # time_matrix <- end_time - start_time
+
+  ###
+
   return(
     list(
       ci_l_loa_l_bt = ci_l_loa_l_bt,
