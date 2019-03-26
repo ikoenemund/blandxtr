8caa7b167a438ae55cc5c12fc73aa88675ac09c3
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-03-26 11:36:06 +0100

    Script for testing analysis with some testdata

---
+++ R/blandxtrMain.testdata.R
@@ -0,0 +1,38 @@
+# CardiacOutput
+
+start_time <- Sys.time()
+
+source("R/blandxtr.readData.R")
+path <- "D:/EigeneDateien/Studium/MedizinischeInformatik/Bachelorarbeit/IMBS/BA_Bland_Altman/R_BA_BlandAltman/Daten_BA/CardiacOutput.csv"
+input_dt <- blandxtr_readData(path)
+
+source("R/blandxtrMain.R")
+biasMod <- FALSE
+bt <- 10
+
+cardiacOutput_result <- blandxtrMain (input_dt, bt, biasMod)
+biasMod <- cardiacOutput_result$res$biasMod
+
+end_time <- Sys.time()
+time_total <- end_time - start_time
+time_total
+
+# ----------------------------------
+# Saturation
+
+start_time <- Sys.time()
+
+source("R/blandxtr.readData.R")
+path <- "D:/EigeneDateien/Studium/MedizinischeInformatik/Bachelorarbeit/IMBS/BA_Bland_Altman/R_BA_BlandAltman/Daten_BA/Saturation.csv"
+input_dt <- blandxtr_readData(path)
+
+source("R/blandxtrMain.R")
+biasMod <- TRUE
+bt <- 0
+
+saturation_result <- blandxtrMain (input_dt, bt, biasMod)
+biasMod <- saturation_result$res$biasMod
+
+end_time <- Sys.time()
+time_total <- end_time - start_time
+time_total
