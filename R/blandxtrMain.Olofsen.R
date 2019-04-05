73f85d429da19246865526fa1e591cc2e6a168f2
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-04-05 10:31:39 +0200

    Minor changes

--- R/blandxtrMain.Olofsen.R
+++ R/blandxtrMain.Olofsen.R
@@ -1,18 +1,21 @@
 # Modified Bland Altman-analysis on data from Olofsen et al. (2015).
 # source (web): https://sec.lumc.nl/method_agreement_analysis/sim.txt
 
-library(profvis)
-profvis({
+# library(profvis)
+# profvis({
 
-  source("R/blandxtr.readData.R")
-  path <- "data/dataOlofsen.csv"
-  input_dt <- blandxtr_readData(path)
+  # get data.table from RData
+  source("R/loadRData.R")
+  input_dt <- loadRData("data/dataOlofsen.RData")
+
+  alpha <- 0.05
+  beta <- 0.05
 
   source("R/blandxtrMain.R")
   biasMod <- TRUE
   bt <- 10
 
-  olofsen_result <- blandxtrMain (input_dt, bt, biasMod)
+  olofsen_result <- blandxtrMain (input_dt, bt, biasMod, alpha, beta)
   biasMod <- olofsen_result$res$biasMod
 
-})
+# })
