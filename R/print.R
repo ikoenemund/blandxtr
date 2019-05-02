0f946ca4ce1589c56fe96ccbc3095d21686d78ec
ikoenemund <46993373+ikoenemund@users.noreply.github.com>
2019-05-02 12:26:51 +0200

    Added SE of Bias in print. Added return values from modified true value varies method.

--- R/print.R
+++ R/print.R
@@ -13,8 +13,26 @@
 #' @export
 
 print.blandxtr <- function (x, ...) {
-  cat("Main results from modified Bland Altman-analysis: \n
-    Bias: ", x$bv$d, "(SE: ", x$bv$d, ")\n",
-    sprintf("Limits of agreement: [%.2f, %.2f]", x$loa$loa_l, x$loa$loa_u))
-  invisible(x)
+  if(x$bias_alt){
+    cat("Main results from modified Bland Altman-analysis: \n
+    Using standard true value varies method \n
+    Bias: ", x$bv$d_a, "(SE: ", x$var_loa$se_d, ")\n",
+      sprintf("Limits of agreement: [%.2f, %.2f]", x$loa$loa_l, x$loa$loa_u),
+    "\n\n  (Using modified true value varies method) \n
+    Bias: ", x$bv$d_a, "(SE: ", x$var_loa_mod$se_d, ")\n",
+      sprintf("Limits of agreement: [%.2f, %.2f]", x$loa_mod$loa_l,
+        x$loa_mod$loa_u))
+    invisible(x)
+  } else {
+    cat("Main results from modified Bland Altman-analysis: \n
+    Using standard true value varies method \n
+    Bias: ", x$bv$d, "(SE: ", x$var_loa$se_d, ")\n",
+      sprintf("Limits of agreement: [%.2f, %.2f]", x$loa$loa_l, x$loa$loa_u),
+      "\n (Using modified true value varies method) \n
+    Bias: ", x$bv$d_a, "(SE: ", x$var_loa_mod$se_d, ")\n",
+      sprintf("Limits of agreement: [%.2f, %.2f]", x$loa_mod$loa_l,
+        x$loa_mod$loa_u))
+    invisible(x)
+  }
+
 }
